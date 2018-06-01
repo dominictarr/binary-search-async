1293367892de37cef20bb864667f6da79f1688c2
Dominic Tarr <dominic.tarr@gmail.com>
2018-06-01 22:05:29 +1200

    switch to tape, because it provides better debugging output

--- test.js
+++ test.js
@@ -1,46 +1,71 @@
-var expect = require("chai").expect;
+var tape = require('tape')
 
-describe("binarysearch", function() {
   var bs  = require("./"),
       arr = [1, 2, 2, 2, 3, 5, 9],
       cmp = function(a, b) { return a - b; };
 
-  it("should bail if not passed an array", function() {
-    expect(function() { bs(undefined, 3, cmp); }).to.throw(TypeError);
+  var lo = 0, hi = arr.length
+
+  function get (i, cb) {
+    return cb(null, arr[i])
+  }
+
+  function cb (err, value) {
+    if(err) throw err
+    return value
+  }
+
+  tape("bail if not passed an array", function(t) {
+    t.throws(function() { bs(undefined, 3, cmp, lo, hi, cb); })
+    t.end()
   });
 
-  it("should bail if not passed a comparator", function() {
-    expect(function() { bs(arr, 3, undefined); }).to.throw(TypeError);
+  tape("bail if not passed a comparator", function(t) {
+    t.throws(function() { bs(get, 3, undefined, lo, hi, cb); })
+    t.end()
   });
 
-  it("should return the index of an item in a sorted array", function() {
-    expect(bs(arr, 3, cmp)).to.equal(4);
+  tape("return the index of an item in a sorted array", function(t) {
+    t.equal(bs(get, 3, cmp, lo, hi, cb), 4)
+    t.end()
   });
 
-  it("should return the index of where the item would go plus one, negated, if the item is not found", function() {
-    expect(bs(arr, 4, cmp)).to.equal(-6);
+  tape("return the index of where the item would go plus one, negated, if the item is not found", function(t) {
+    t.equal(bs(get, 4, cmp, lo, hi, cb), -6);
+    t.end()
   });
 
-  it("should return any valid index if an item exists multiple times in the array", function() {
-    expect(bs(arr, 2, cmp)).to.equal(3);
+  tape("return any valid index if an item exists multiple times in the array", function(t) {
+    t.equal(bs(get, 2, cmp, lo, hi, cb), 3);
+    t.end()
   });
 
-  it("should work even on empty arrays", function() {
-    expect(bs([], 42, cmp)).to.equal(-1);
+  tape("work even on empty lookups", function(t) {
+
+    t.throws(function () {
+      bs(function (_, cb) { return cb(new Error()) }, 42, cmp, lo, hi, cb)
+    })
+    t.end()
   });
 
-  it("should work even on arrays of doubles", function() {
-    expect(bs([0.0, 0.1, 0.2, 0.3, 0.4], 0.25, cmp)).to.equal(-4);
+  tape("work even on arrays of doubles", function(t) {
+    var decimals = [0.0, 0.1, 0.2, 0.3, 0.4]
+    t.equal(bs(function (i, cb) {
+      return cb(null, decimals[i])
+    }, 0.25, cmp, 0, 5, cb), -4);
+    t.end()
   });
 
-  it("should pass the index and array parameters to the comparator", function() {
+  tape("pass the index and array parameters to the comparator", function(t) {
     var indexes = [],
-        indexCmp = function(a, b, i, array) {
-          expect(array).to.equal(arr);
+        indexCmp = function(a, b, i) {
           indexes.push(i);
           return cmp(a, b);
         };
-    bs(arr, 3, indexCmp);
-    expect(indexes).to.deep.equal([3, 5, 4])
+    bs(get, 3, indexCmp, 0, arr.length, cb);
+    t.deepEqual(indexes,[3, 5, 4])
+    t.end()
   });
-});
+
+
+
